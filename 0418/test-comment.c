6a490abd6de198015cb3e852bfa1dc446eeaf959
Efuwa <ikeda-kao120@g.ecc.u-tokyo.ac.jp>
2022-04-27 01:33:23 +0900

    0418

--- 0418/test-comment.c
+++ 0418/test-comment.c
@@ -1,10 +1,12 @@
 /* test0.c */
 #include <stdio.h>
 /*
-* このファイルを改変し適切なコメント(10行以上）を付けること
+* 引数：整数i, j
+* 返値：iとjの積となる整数
+* 機能：引き算の積を計算し返す関数
 */
-int test(int i, int j) {
-  return (i * j);
+int test(int i, int j) { /*入力となる整数i, j */
+  return (i * j); /* iとjの積 */
 }
 
 int main(int argc, char *argv) {
@@ -12,7 +14,8 @@ int main(int argc, char *argv) {
   int k;
   i = 3;
   j = 2;
-  k = test(i, j);
+  k = test(i, j); /* iとjをかけてkに代入する */
+  /* kの値が5より大きければ">5"と表示し、そうでなければ"<=5"を表示する */
   if (k > 5) {
     printf(">5\n");
   } else {
